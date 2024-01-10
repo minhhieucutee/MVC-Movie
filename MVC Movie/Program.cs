060f5c824cd2295e3f90160c2df5613e8809ea92
Hoàng Minh Hiếu <111435941+minhhieucutee@users.noreply.github.com>
2024-01-10 14:44:47 +0700

    Add files via upload

---
+++ MVC Movie/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
+
+var app = builder.Build();
+
+// Seeding data
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    // Delete db with seeded data
+    //var dbContext = services.GetRequiredService<MvcMovieContext>();
+
+    // Xóa dữ liệu hiện tại
+    //dbContext.Database.EnsureDeleted();
+
+    // Tạo lại cơ sở dữ liệu nếu cần
+    //dbContext.Database.EnsureCreated();
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+// The routing format is set in the Program.cs file.
+app.MapControllerRoute(
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
